248283aa2f6897cb44b870d473eca35e40824714
NIST\crosin <caroline.rosin@nist.gov>
2019-08-13 13:25:37 -0400

    junit tests

---
+++ src/test/java/gov/nist/healthcare/elr/custom/cs/TestELR_021.java
@@ -0,0 +1,90 @@
+package gov.nist.healthcare.elr.custom.cs;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+
+import org.apache.commons.collections4.SetUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestELR_021 {
+
+	/**
+	 * ELR-021: MSH.21.1 (Entity Identifier) of an occurrence of MSH.21 (Message
+	 * Profile Identifier) SHALL be valued with 'PHLabReport-Ack' OR
+	 * 'PHLabReport-NoAck' OR 'PHLabReport-Batch'
+	 */
+
+	private static ELR_021 testObject;
+
+	@BeforeClass
+	public static void setUp() {
+		testObject = new ELR_021();
+	}
+
+	@Test
+	public void testCheckSuccess() {
+		HashSet<String> actual = SetUtils.hashSet("PHLabReport-Ack");
+		boolean result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("PHLabReport-NoAck");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("PHLabReport-Batch");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("ABC", "PHLabReport-Ack");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("ABC", "PHLabReport-NoAck");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("ABC", "PHLabReport-Batch");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		/*
+		 * With current implementation, this is valid. May change in the future.
+		 */
+		actual = SetUtils.hashSet("PHLabReport-Ack", "PHLabReport-NoAck", "PHLabReport-Batch");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("PHLabReport-Ack", "PHLabReport-Ack");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("PHLabReport-NoAck", "PHLabReport-NoAck");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("PHLabReport-Batch", "PHLabReport-Batch");
+		result = testObject.check(actual);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testCheckFail() {
+		HashSet<String> actual = SetUtils.hashSet("");
+		boolean result = testObject.check(actual);
+		assertFalse(result);
+
+		actual = SetUtils.hashSet("ABC");
+		result = testObject.check(actual);
+		assertFalse(result);
+
+		actual = SetUtils.hashSet("ABC", "DEF");
+		result = testObject.check(actual);
+		assertFalse(result);
+
+		result = testObject.check(null);
+		assertFalse(result);
+	}
+}
---
+++ src/test/java/gov/nist/healthcare/elr/custom/cs/TestELR_022.java
@@ -0,0 +1,62 @@
+package gov.nist.healthcare.elr.custom.cs;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+
+import org.apache.commons.collections4.SetUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestELR_022 {
+
+	/**
+	 * ELR-022: MSH-21.3 (Universal ID) of an occurrence of MSH.21 (Message Profile
+	 * Identifier) SHALL contain the value "2.16.840.1.113883.9.11"
+	 */
+
+	private static ELR_022 testObject;
+
+	@BeforeClass
+	public static void setUp() {
+		testObject = new ELR_022();
+	}
+
+	@Test
+	public void testCheckSuccess() {
+		HashSet<String> actual = SetUtils.hashSet("2.16.840.1.113883.9.11");
+		boolean result = testObject.check(actual);
+		assertTrue(result);
+
+		actual = SetUtils.hashSet("ABC", "2.16.840.1.113883.9.11");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+		/*
+		 * With current implementation, this is valid. May change in the future.
+		 */
+		actual = SetUtils.hashSet("2.16.840.1.113883.9.11", "2.16.840.1.113883.9.11");
+		result = testObject.check(actual);
+		assertTrue(result);
+
+	}
+
+	@Test
+	public void testCheckFail() {
+		HashSet<String> actual = SetUtils.hashSet("");
+		boolean result = testObject.check(actual);
+		assertFalse(result);
+
+		actual = SetUtils.hashSet("ABC");
+		result = testObject.check(actual);
+		assertFalse(result);
+
+		actual = SetUtils.hashSet("ABC", "DEF");
+		result = testObject.check(actual);
+		assertFalse(result);
+
+		result = testObject.check(null);
+		assertFalse(result);
+	}
+}
