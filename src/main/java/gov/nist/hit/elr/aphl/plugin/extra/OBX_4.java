709ebd8aed9a3691418b5925fa3db289661113a2
NIST\crosin <caroline.rosin@nist.gov>
2019-09-06 14:14:32 -0400

    support for OBX-4 validation

---
+++ src/main/java/gov/nist/hit/elr/aphl/plugin/extra/OBX_4.java
@@ -0,0 +1,104 @@
+package gov.nist.hit.elr.aphl.plugin.extra;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import gov.nist.hit.elr.plugin.utils.CodedElement;
+import hl7.v2.instance.Element;
+import hl7.v2.instance.Query;
+import hl7.v2.instance.Simple;
+import scala.collection.Iterator;
+import scala.collection.immutable.List;
+
+public class OBX_4 {
+
+	/**
+	 * OBX-4 condition predicate/conformance statement : If there are multiple OBX
+	 * segments associated with the same OBR segment that have the same OBX-3 values
+	 * for (OBX-3.1 and OBX-3.3) or (OBX-3.4 and OBX-3.6).
+	 */
+
+	/**
+	 * 
+	 * @param e the ORDER_OBSERVATION group
+	 * @return
+	 */
+	public java.util.List<String> assertionWithCustomMessages(Element e) {
+		java.util.List<String> messages = new java.util.ArrayList<String>();
+
+		List<Element> OBXList = Query.query(e, "6[*].1[1]").get();
+		if (OBXList == null || OBXList.size() == 0) {
+			// no OBX, we can move on, no check performed
+			return messages;
+		}
+		java.util.List<CodedElement> _OBX3s = new ArrayList<CodedElement>();
+		Iterator<Element> it = OBXList.iterator();
+		while (it.hasNext()) {
+
+			Element OBX = it.next();
+			// OBX-3
+			List<Simple> OBX3_1List = Query.queryAsSimple(OBX, "3[1].1[1]").get();
+			List<Simple> OBX3_3List = Query.queryAsSimple(OBX, "3[1].3[1]").get();
+			List<Simple> OBX3_4List = Query.queryAsSimple(OBX, "3[1].4[1]").get();
+			List<Simple> OBX3_6List = Query.queryAsSimple(OBX, "3[1].6[1]").get();
+
+			String OBX3_1 = OBX3_1List.size() > 0 ? OBX3_1List.apply(0).value().raw() : "";
+			String OBX3_3 = OBX3_3List.size() > 0 ? OBX3_3List.apply(0).value().raw() : "";
+			String OBX3_4 = OBX3_4List.size() > 0 ? OBX3_4List.apply(0).value().raw() : "";
+			String OBX3_6 = OBX3_6List.size() > 0 ? OBX3_6List.apply(0).value().raw() : "";
+
+			CodedElement identifierOBX3 = new CodedElement(OBX3_1, OBX3_3);
+			CodedElement alternateOBX3 = new CodedElement(OBX3_4, OBX3_6);
+
+			if (!identifierOBX3.isEmpty()) {
+				_OBX3s.add(identifierOBX3);
+			}
+			if (!alternateOBX3.isEmpty()) {
+				_OBX3s.add(alternateOBX3);
+			}
+
+		}
+		Iterator<Element> it2 = OBXList.iterator();
+		while (it2.hasNext()) {
+
+			Element OBX = it2.next();
+			// OBX-3
+			List<Simple> OBX3_1List = Query.queryAsSimple(OBX, "3[1].1[1]").get();
+			List<Simple> OBX3_3List = Query.queryAsSimple(OBX, "3[1].3[1]").get();
+			List<Simple> OBX3_4List = Query.queryAsSimple(OBX, "3[1].4[1]").get();
+			List<Simple> OBX3_6List = Query.queryAsSimple(OBX, "3[1].6[1]").get();
+
+			String OBX3_1 = OBX3_1List.size() > 0 ? OBX3_1List.apply(0).value().raw() : "";
+			String OBX3_3 = OBX3_3List.size() > 0 ? OBX3_3List.apply(0).value().raw() : "";
+			String OBX3_4 = OBX3_4List.size() > 0 ? OBX3_4List.apply(0).value().raw() : "";
+			String OBX3_6 = OBX3_6List.size() > 0 ? OBX3_6List.apply(0).value().raw() : "";
+
+			CodedElement identifierOBX3 = new CodedElement(OBX3_1, OBX3_3);
+			CodedElement alternateOBX3 = new CodedElement(OBX3_4, OBX3_6);
+
+			// OBX-4
+			List<Simple> OBX4List = Query.queryAsSimple(OBX, "4[1]").get();
+			String OBX4 = OBX4List.size() > 0 ? OBX4List.apply(0).value().raw() : "";
+
+			if (!identifierOBX3.isEmpty()) {
+				messages.addAll(check(_OBX3s, identifierOBX3, OBX4));
+			}
+			if (!alternateOBX3.isEmpty()) {
+				messages.addAll(check(_OBX3s, alternateOBX3, OBX4));
+			}
+		}
+		return messages;
+	}
+
+	public java.util.List<String> check(java.util.List<CodedElement> obx3s, CodedElement obx3, String obx4) {
+		java.util.List<String> messages = new java.util.ArrayList<String>();
+		int count = Collections.frequency(obx3s, obx3);
+		if (count > 1 && "".equals(obx4)) {
+			messages.add("OBX-4 is required with OBX-3 (" + obx3.prettyPrint() + ")");
+//			messages.add(
+//					"The combination of OBX-3 (" + obx3.prettyPrint() + ") and OBX-4 (" + obx4 + ") is not unique");
+
+		}
+		return messages;
+	}
+}
---
+++ src/test/java/gov/nist/hit/elr/aphl/plugin/extra/TestOBX_4.java
@@ -0,0 +1,156 @@
+package gov.nist.hit.elr.aphl.plugin.extra;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import gov.nist.hit.elr.plugin.util.Util;
+import gov.nist.hit.elr.plugin.utils.CodedElement;
+import gov.nist.validation.report.Entry;
+import gov.nist.validation.report.Report;
+import hl7.v2.validation.SyncHL7Validator;
+
+public class TestOBX_4 {
+
+	private static OBX_4 testObject;
+
+	private static List<CodedElement> OBX3s;
+
+	@BeforeClass
+	public static void setUp() {
+		testObject = new OBX_4();
+		OBX3s = new ArrayList<CodedElement>();
+	}
+
+	@Before
+	public void resetData() {
+		OBX3s.removeAll(OBX3s);
+	}
+
+	@Test
+	public void testCheckSuccess() {
+		CodedElement ce1 = new CodedElement("ABC", "CS1");
+		CodedElement ce2 = new CodedElement("DEF", "CS2");
+		CodedElement ce3 = new CodedElement("GHI", "CS1");
+
+		OBX3s.add(ce1);
+		OBX3s.add(ce2);
+		OBX3s.add(ce3);
+
+		// OBX-3 are all unique, no OBX-4
+		List<String> result = testObject.check(OBX3s, ce1, "");
+		assertEquals(0, result.size());
+
+		result = testObject.check(OBX3s, ce2, "");
+		assertEquals(0, result.size());
+
+		result = testObject.check(OBX3s, ce3, "");
+		assertEquals(0, result.size());
+
+		// OBX-3 are all unique, with a value OBX-4
+		result = testObject.check(OBX3s, ce1, "1");
+		assertEquals(0, result.size());
+
+		result = testObject.check(OBX3s, ce2, "2");
+		assertEquals(0, result.size());
+
+		result = testObject.check(OBX3s, ce3, "z");
+		assertEquals(0, result.size());
+
+		// Multiple OBX-3, with a value OBX-4
+		OBX3s.add(ce1);
+		OBX3s.add(ce2);
+		OBX3s.add(ce3);
+
+		result = testObject.check(OBX3s, ce1, "1");
+		assertEquals(Collections.frequency(OBX3s, ce1), 2);
+		assertEquals(0, result.size());
+
+		result = testObject.check(OBX3s, ce2, "2");
+		assertEquals(Collections.frequency(OBX3s, ce2), 2);
+		assertEquals(0, result.size());
+
+		result = testObject.check(OBX3s, ce3, "z");
+		assertEquals(Collections.frequency(OBX3s, ce3), 2);
+		assertEquals(0, result.size());
+	}
+
+	@Test
+	public void testCheckFail() {
+		CodedElement ce1 = new CodedElement("ABC", "CS1");
+		CodedElement ce2 = new CodedElement("DEF", "CS2");
+		CodedElement ce3 = new CodedElement("GHI", "CS1");
+
+		OBX3s.add(ce1);
+		OBX3s.add(ce2);
+		OBX3s.add(ce3);
+		OBX3s.add(ce1);
+		OBX3s.add(ce2);
+		OBX3s.add(ce3);
+
+		// Multiple OBX-3, no OBX-4
+		List<String> result = testObject.check(OBX3s, ce1, "");
+		assertEquals(Collections.frequency(OBX3s, ce1), 2);
+		assertEquals(1, result.size());
+
+		result = testObject.check(OBX3s, ce2, "");
+		assertEquals(Collections.frequency(OBX3s, ce2), 2);
+		assertEquals(1, result.size());
+
+		result = testObject.check(OBX3s, ce3, "");
+		assertEquals(Collections.frequency(OBX3s, ce3), 2);
+		assertEquals(1, result.size());
+
+	}
+
+	@Test
+	public void testMessage() throws Exception {
+
+		String globalFolder = "/MessageProfile";
+
+		String profiles = StringUtils.join(globalFolder, "/Profile.xml");
+		String constraints = StringUtils.join(globalFolder, "/Constraints.xml");
+		String valueSets = StringUtils.join(globalFolder, "/ValueSets.xml");
+
+		String message1FileName = "TestMessages/OBX4/Message1.txt";
+
+		SyncHL7Validator validator = Util.createValidator(profiles, constraints, null, valueSets);
+		ClassLoader classLoader = getClass().getClassLoader();
+		File message1 = new File(classLoader.getResource(message1FileName).getFile());
+		String messageString = FileUtils.readFileToString(message1);
+		Report report = validator.check(messageString, "ORU_R01");
+
+		Set<String> keys = report.getEntries().keySet();
+		int errors = 0;
+		int alerts = 0;
+		for (String key : keys) {
+			List<Entry> entries = report.getEntries().get(key);
+			if (entries != null && entries.size() > 0) {
+				System.out.println("*** " + key + " ***");
+				for (Entry entry : entries) {
+					switch (entry.getClassification()) {
+					case "Error":
+						Util.printEntry(entry);
+						errors++;
+						break;
+					case "Alert":
+						Util.printEntry(entry);
+						alerts++;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+}
